82de463c715e4b9e814a4fcb8a253a5c2ec2be3f
Ryan Roden-Corrent <ryan@rcorre.net>
2016-02-18 21:30:21 -0500

    Draw a grid and rectangles in the demo.
    Place a green rectangle when the mouse is clicked.

--- demo/src/app.d
+++ demo/src/app.d
@@ -1,14 +1,25 @@
 import std.stdio;
-import discord;
-import backend;
+import std.range;
+
 import allegro5.allegro;
 import allegro5.allegro_color;
 import allegro5.allegro_primitives;
+
+import discord;
+import backend;
+
+enum {
+    screenW = 800,
+    screenH = 600,
+    gridSpace = 20,
+}
 
 int main() {
     ALLEGRO_DISPLAY     *display     = null;
     ALLEGRO_EVENT_QUEUE *event_queue = null;
     ALLEGRO_TIMER       *timer       = null;
+
+    Shape!float[] shapes;
 
     if(!al_init()) {
         stderr.writeln("failed to initialize allegro!\n");
@@ -23,7 +34,7 @@ int main() {
         return -1;
     }
 
-    display = al_create_display(640, 480);
+    display = al_create_display(screenW, screenH);
     if(!display) {
         stderr.writeln("failed to create display!\n");
         al_destroy_timer(timer);
@@ -38,8 +49,13 @@ int main() {
         return -1;
     }
 
+    al_install_mouse();
+    al_install_keyboard();
+
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
 
     al_start_timer(timer);
 
@@ -54,10 +70,20 @@ int main() {
         else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             break;
         }
+        else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+            immutable x = ev.mouse.x;
+            immutable y = ev.mouse.y;
+            shapes ~= shape(box2f(x, y, x + 20, y + 20));
+        }
 
         if(redraw && al_is_event_queue_empty(event_queue)) {
             redraw = false;
             al_clear_to_color(al_map_rgb(0,0,0));
+
+            drawGrid();
+
+            foreach(shape ; shapes)
+                shape.visitAny!(x => draw(x, al_map_rgb(0, 255, 0)));
 
             al_flip_display();
         }
@@ -68,4 +94,14 @@ int main() {
     al_destroy_event_queue(event_queue);
 
     return 0;
+}
+
+void drawGrid() {
+    immutable color = al_map_rgb(100, 100, 100);
+
+    foreach(x ; 0.iota(screenW, gridSpace))
+        draw(seg2f(vec2f(x, -10), vec2f(x, screenH + 10)), color);
+
+    foreach(y ; 0.iota(screenH, gridSpace))
+        draw(seg2f(vec2f(-10, y), vec2f(screenW + 10, y)), color);
 }
--- demo/src/backend.d
+++ demo/src/backend.d
@@ -5,17 +5,13 @@ import allegro5.allegro_primitives;
 
 import discord;
 
-private enum dotRadius = 2;    // radius of dots representing points
-private enum thickness = 2;    // general thickness
-private enum rayThickness = 1; // thickness for rays
 private enum rayLength = 2000; // how far to extend a ray's 'beam'
 
-
-void draw(T)(Vector!(T,2) v, ALLEGRO_COLOR color) {
-	al_draw_filled_circle(v.x, v.y, dotRadius, color); // draw a point
+void draw(T)(Vector!(T,2) v, ALLEGRO_COLOR color, float radius = 2) {
+	al_draw_filled_circle(v.x, v.y, radius, color); // draw a point
 }
 
-void draw(T)(Ray!(T,2) r, ALLEGRO_COLOR color) {
+void draw(T)(Ray!(T,2) r, ALLEGRO_COLOR color, float thickness = 1) {
     immutable start = r.orig;
     immutable end = start + r.dir.normalized * rayLength;
 
@@ -23,10 +19,10 @@ void draw(T)(Ray!(T,2) r, ALLEGRO_COLOR color) {
 	draw(start, color);
 
     // draw a segment extending from the start
-	al_draw_line(start.x, start.y, end.x, end.y, color, rayThickness);
+	al_draw_line(start.x, start.y, end.x, end.y, color, thickness);
 }
 
-void draw(T)(Segment!(T,2) s, ALLEGRO_COLOR color) {
+void draw(T)(Segment!(T,2) s, ALLEGRO_COLOR color, float thickness = 1) {
     // dot the endpoints
 	draw(s.a, color);
 	draw(s.b, color);
@@ -35,18 +31,14 @@ void draw(T)(Segment!(T,2) s, ALLEGRO_COLOR color) {
 	al_draw_line(s.a.x, s.a.y, s.b.x, s.b.y, color, thickness);
 }
 
-void draw(T)(Ray!(T,2) s, ALLEGRO_COLOR color) {
-	al_draw_line(s.a.x, s.a.y, s.b.x, s.b.y, color, thickness);
-}
-
-void draw(T)(Box!(T, 2) b, ALLEGRO_COLOR color) {
+void draw(T)(Box!(T, 2) b, ALLEGRO_COLOR color, float thickness = 1) {
 	al_draw_rectangle(b.min.x, b.min.y, b.max.x, b.max.y, color, thickness);
 }
 
-void draw(T)(Sphere!(T,2) c, ALLEGRO_COLOR color) {
+void draw(T)(Sphere!(T,2) c, ALLEGRO_COLOR color, float thickness = 1) {
 	al_draw_circle(c.center.x, c.center.y, c.radius, color, thickness);
 }
 
-void draw(T)(Triangle!(T,2) t, ALLEGRO_COLOR color) {
+void draw(T)(Triangle!(T,2) t, ALLEGRO_COLOR color, float thickness = 1) {
 	al_draw_triangle(t.a.x, t.a.y, t.b.x, t.b.y, t.c.x, t.c.y, color, thickness);
 }
