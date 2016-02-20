d1a4ab3432e9fa2275e3edf311d7f401d00bc3c5
Ryan Roden-Corrent <ryan@rcorre.net>
2016-02-19 22:56:58 -0500

    Implement a generic Shape type.

--- src/discord/shape.d
+++ src/discord/shape.d
@@ -7,6 +7,8 @@ import std.variant;
 import gfm.math.box;
 import gfm.math.shapes;
 
+public import std.variant : visit, tryVisit;
+
 /// A generic struct that can hold any one of a number of 2D shapes.
 alias Shape(T) = Algebraic!(Segment!(T, 2),
                             Triangle!(T,2),
@@ -19,10 +21,33 @@ auto shape(T)(T someShape) {
     return Shape!V(someShape);
 }
 
-auto visitAny(alias fn, V)(ref V var) {
-    foreach(T ; var.AllowedTypes)
-        if (auto ptr = var.peek!T)
+/**
+ * Apply the same function (or overload set) to whatever shape this contains.
+ */
+auto visitAny(alias fn, S)(ref S shape) if (isShape!S) {
+    foreach(T ; shape.AllowedTypes)
+        if (auto ptr = shape.peek!T)
             return fn(*ptr);
 
     assert(0, "Variant holds no value");
+}
+
+template isShape(V : VariantN!T, T...) {
+    static if (__traits(compiles, TemplateArgsOf!(T[1])[0])) {
+        alias NumericType = TemplateArgsOf!(T[1])[0]; // e.g. float, int, etc
+        enum isShape = is(V == Shape!NumericType);
+    }
+    else {
+        enum isShape = false;
+    }
+}
+
+template isShape(T) {
+    enum isShape = false;
+}
+
+unittest {
+    static assert(isShape!(Shape!float));
+    static assert(!isShape!(int));
+    static assert(!isShape!(Algebraic!(int, float)));
 }
