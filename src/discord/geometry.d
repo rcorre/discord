46aeae8aac8d0b220d3cc6b7f1993c0a75c4e845
Ryan Roden-Corrent <ryan@rcorre.net>
2016-02-24 21:26:58 -0500

    Add vertices/edges geometry functions.
    A generic `edges` function will be useful for the Separating Axis
    implementation for collision detection.
    
    Also add convenience functions for getting a particular vertex or edge
    of a box (top, topLeft, ect).

--- src/discord/geometry.d
+++ src/discord/geometry.d
@@ -33,21 +33,85 @@ unittest {
     assert(test(seg2f(vec2f( 2, 2), vec2f(-2,-2)), vec2f(-1, 1)));
 }
 
-auto topLeft(box2f box) { return box.min; }
-auto topRight(box2f box) { return vec2f(box.max.x, box.min.y); }
-auto bottomLeft(box2f box) { return vec2f(box.min.x, box.max.y); }
-auto bottomRight(box2f box) { return box.max; }
+/// Return the segment composing an edge of a box
+auto top(T)(Box!(T, 2) b) { return seg2f(b.topLeft, b.topRight); }
+/// ditto
+auto left(T)(Box!(T, 2) b) { return seg2f(b.bottomLeft, b.topLeft); }
+/// ditto
+auto right(T)(Box!(T, 2) b) { return seg2f(b.topRight, b.bottomRight); }
+/// ditto
+auto bottom(T)(Box!(T, 2) b) { return seg2f(b.bottomRight, b.bottomLeft); }
 
-/**
- * Returns a range of the segments composing the sides of a box.
- */
-auto edges(box2f box) {
-    return only(seg2f(box.topLeft, box.topRight),       // top
-                seg2f(box.topRight, box.bottomRight),   // right
-                seg2f(box.bottomRight, box.bottomLeft), // bottom
-                seg2f(box.bottomLeft, box.topLeft));    // left
+///
+unittest {
+    auto box = box2f(0, 2, 4, 8);
+
+    assert(box.top    == seg2f(vec2f(0, 2), vec2f(4, 2)));
+    assert(box.left   == seg2f(vec2f(0, 8), vec2f(0, 2)));
+    assert(box.right  == seg2f(vec2f(4, 2), vec2f(4, 8)));
+    assert(box.bottom == seg2f(vec2f(4, 8), vec2f(0, 8)));
 }
 
+/// Return the given vertex (corner) of a box
+auto topLeft(T)(Box!(T, 2) box) { return box.min; }
+/// Return the given vertex (corner) of a box
+auto topRight(T)(Box!(T, 2) box) { return vec2f(box.max.x, box.min.y); }
+/// Return the given vertex (corner) of a box
+auto bottomLeft(T)(Box!(T, 2) box) { return vec2f(box.min.x, box.max.y); }
+/// Return the given vertex (corner) of a box
+auto bottomRight(T)(Box!(T, 2) box) { return box.max; }
+
+///
+unittest {
+    auto b = box2f(0, 0, 10, 20);
+    assert(b.topLeft     == vec2f(0,0));
+    assert(b.topRight    == vec2f(10,0));
+    assert(b.bottomLeft  == vec2f(0,20));
+    assert(b.bottomRight == vec2f(10,20));
+}
+
+/// Return a range of the vertices of a shape.
+auto vertices(T)(Box!(T, 2) b) {
+    return only(b.topLeft, b.topRight, b.bottomRight, b.bottomLeft);
+}
+
+/// ditto
+auto vertices(T)(Triangle!(T, 2) t) {
+    return only(t.a, t.b, t.c);
+}
+
+///
+unittest {
+    import std.algorithm : equal;
+
+    auto box = box2f(0, 0, 10, 20);
+    assert(box.vertices.equal([vec2f( 0, 0),    // top left
+                               vec2f(10, 0),    // top right
+                               vec2f(10,20),    // bottom right
+                               vec2f( 0,20)])); // bottom left
+
+    // compose a triangle from half of the box
+    auto tri = triangle2f(box.topLeft, box.topRight, box.bottomLeft);
+    assert(tri.vertices.equal([vec2f(0 ,0),    // top left
+                               vec2f(10,0),    // top right
+                               vec2f(0,20)])); // bottom left
+}
+
+/// Returns a range of the segments composing the sides of a shape.
+auto edges(T)(T a) if (is(typeof(a.vertices))) {
+    // we need a function to deduce the proper segment type from the vertex type
+    // for example, given two vec2f, returns a seg2f
+    auto segment(V)(Vector!(V, 2) v1, Vector!(V, 2) v2) {
+        return Segment!(V, 2)(v1, v2);
+    }
+
+    // create edges from the chain [v0 -> v1, v1 -> v2, ... , vn -> v0]
+    auto v = a.vertices;
+    return v.zip(v.drop(1).chain(v.front.only))
+        .map!(pair => segment(pair[0], pair[1]));
+}
+
+///
 unittest {
     import std.algorithm : map, equal;
 
@@ -67,4 +131,15 @@ unittest {
     ];
 
     assert(actualNormals.equal(expectedNormals));
+}
+
+/**
+ * Returns a range containing the segment itself.
+ */
+auto edges(T)(Segment!(T, 2) seg) { return only(seg); }
+
+unittest {
+    auto seg = seg2f(vec2f(10, 10), vec2f(20, 20));
+    assert(seg.edges.front == seg);
+    assert(seg.edges.length == 1);
 }
